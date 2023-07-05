8517d4f13b13c6b05d578001b447ba6b3cc8a223
Pream Pinbut <momozahara@hotmail.com>
2023-07-06 01:58:27 +0700

    Update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,15 +3,11 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
-local components = require("lvim.core.lualine.components")
-lvim.builtin.lualine.sections.lualine_a = {
-  components.mode,
-  "mode"
-}
-
 vim.opt.relativenumber = true
 vim.opt.wrap = true
 vim.opt.showcmd = true
+
+lvim.format_on_save = true
 
 -- Buffer navigation
 lvim.keys.normal_mode["<Tab>"] = ":bnext<cr>"
@@ -24,78 +20,7 @@ lvim.keys.normal_mode["<leader>W"] = ":wa<cr>"
 vim.api.nvim_set_keymap('t', '<C-\\><C-n>', "<C-\\><C-n><cr>",
   { noremap = true, silent = true })
 
-lvim.format_on_save = true
-
-lvim.plugins = {
-  {
-    "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp"
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
-}
-
-require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.expand("$HOME/.config/Code/User/snippets") } })
-
-require("lvim.lsp.manager").setup("tsserver", {
-  settings = {
-    diagnostics = { ignoredCodes = { 6133 } }
-  },
-})
-
-require("lvim.lsp.manager").setup("rust_analyzer", {
-  settings = {
-    ["rust-analyzer"] = {
-      lens = {
-        run = {
-          enable = false
-        },
-        debug = {
-          enable = false
-        }
-      }
-    }
-  }
-})
-
-local dap = require('dap')
-dap.adapters.lldb = {
-  type = 'executable',
-  command = '/usr/bin/lldb-vscode',
-  name = 'lldb'
-}
-
-require('dap.ext.vscode').load_launchjs(nil, { lldb = { 'rust' } })
-
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    exe = "eslint_d",
-    filetypes = {
-      "typescript",
-      "typescriptreact",
-      "javascript",
-      "javascriptreact"
-    }
-  }
-}
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  {
-    command = "eslint_d",
-    filetypes = {
-      "typescript",
-      "typescriptreact",
-      "javascript",
-      "javascriptreact"
-    }
-  }
-}
+require("plugins.dap")
+require("plugins.lsp")
+require("plugins.lualine")
+require("plugins.snippet")
