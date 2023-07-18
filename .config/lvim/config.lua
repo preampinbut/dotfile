0f237ee6d9ed9c3951b7addea97114338e086cc4
Pream Pinbut <momozahara@hotmail.com>
2023-07-19 05:16:01 +0700

    Update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -27,7 +27,7 @@ vim.api.nvim_set_keymap('t', '<C-\\><C-n>', "<C-\\><C-n><cr>",
 lvim.colorscheme = "dracula"
 
 lvim.builtin.treesitter.ensure_installed = "all"
-lvim.builtin.which_key.setup.plugins.presets.z = true
+
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
@@ -36,9 +36,33 @@ vim.cmd [[
 augroup remember_folds
     autocmd!
     autocmd BufWinLeave *.* mkview
-    autocmd BufWinEnter *.* silent loadview
+    " autocmd BufWinEnter *.* silent! loadview
 augroup END
 ]]
+
+function Mkview_if_no_view()
+  local view_file = vim.fn.expand('%:p') .. '.view'
+  if vim.fn.filereadable(view_file) == 0 then
+    vim.cmd('mkview')
+  else
+    print('View file already exists.')
+  end
+end
+
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function()
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      once = true,
+      callback = function()
+        vim.defer_fn(function()
+          local str = vim.api.nvim_replace_termcodes("<cmd>lua Mkview_if_no_view()<cr>:silent! loadview<cr>", true, false,
+            true)
+          vim.api.nvim_feedkeys(str, "m", false)
+        end, 60)
+      end,
+    })
+  end,
+})
 
 lvim.plugins = {
   {
@@ -81,3 +105,4 @@ require("plugins.snippet")
 require("plugins.tree")
 require("plugins.telescope")
 require("plugins.comment")
+require("plugins.fold")
