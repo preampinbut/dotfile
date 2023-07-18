ecbddab8449e4ad02b4d7a99a525cce853aea57d
Pream Pinbut <momozahara@hotmail.com>
2023-07-19 04:51:03 +0700

    Update telescope.lua

--- .config/lvim/lua/plugins/telescope.lua
+++ .config/lvim/lua/plugins/telescope.lua
@@ -1,3 +1,9 @@
+local actions = require "telescope.actions"
 lvim.builtin.telescope.defaults.file_ignore_patterns = {
   "node_modules",
 }
+-- NOTE: Fix autocmd loadview not working when open with telescope
+lvim.builtin.telescope.defaults.mappings.i["<CR>"] = function(prompt_bufnr)
+  vim.cmd.stopinsert()
+  vim.defer_fn(function() actions.select_default(prompt_bufnr) end, 10)
+end
