96ca48ee9221126d0e84502b86dcc13c42470dff
Pream Pinbut <momozahara@hotmail.com>
2023-07-19 05:31:15 +0700

    Update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -28,42 +28,6 @@ lvim.colorscheme = "dracula"
 
 lvim.builtin.treesitter.ensure_installed = "all"
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 99
-vim.opt.foldenable = true
-vim.cmd [[
-augroup remember_folds
-    autocmd!
-    autocmd BufWinLeave *.* mkview
-    " autocmd BufWinEnter *.* silent! loadview
-augroup END
-]]
-
-function Mkview_if_no_view()
-  local view_file = vim.fn.expand('%:p') .. '.view'
-  if vim.fn.filereadable(view_file) == 0 then
-    vim.cmd('mkview')
-  else
-    print('View file already exists.')
-  end
-end
-
-vim.api.nvim_create_autocmd("BufRead", {
-  callback = function()
-    vim.api.nvim_create_autocmd("BufWinEnter", {
-      once = true,
-      callback = function()
-        vim.defer_fn(function()
-          local str = vim.api.nvim_replace_termcodes("<cmd>lua Mkview_if_no_view()<cr>:silent! loadview<cr>", true, false,
-            true)
-          vim.api.nvim_feedkeys(str, "m", false)
-        end, 60)
-      end,
-    })
-  end,
-})
-
 lvim.plugins = {
   {
     "folke/todo-comments.nvim",
