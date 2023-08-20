128756ca838c701d849f9b99586888e77ba95cc0
Pream Pinbut <momozahara@hotmail.com>
2023-08-20 11:31:20 +0700

    Update lualine.lua

--- .config/lvim/lua/plugins/lualine.lua
+++ .config/lvim/lua/plugins/lualine.lua
@@ -3,4 +3,8 @@ lvim.builtin.lualine.sections.lualine_a = {
   components.mode,
   "mode"
 }
-lvim.builtin.bufferline.options.mode = "tabs"
+
+lvim.builtin.bufferline.active = true
+lvim.builtin.bufferline.options.mode = "buffers"
+lvim.builtin.bufferline.options.show_close_icon = false
+lvim.builtin.bufferline.options.show_buffer_close_icons = false
