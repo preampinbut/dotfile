7bd6c394a675a85e7aae2d255a93a57e32f37e5b
Pream Pinbut <momozahara@hotmail.com>
2023-11-04 23:47:06 +0700

    Update style-switch.sh

--- .config/polybar/forest/scripts/style-switch.sh
+++ .config/polybar/forest/scripts/style-switch.sh
@@ -5,11 +5,12 @@ SDIR="$HOME/.config/polybar/forest/scripts"
 # Launch Rofi
 MENU="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p '' \
 -theme $SDIR/rofi/styles.rasi \
-<<< " Default| Nord| Gruvbox| Dark| Cherry|")"
+<<< " Default| Nord| Gruvbox| Dark| Cherry| CherryEdit|")"
             case "$MENU" in
 				*Default) "$SDIR"/styles.sh --default ;;
 				*Nord) "$SDIR"/styles.sh --nord ;;
 				*Gruvbox) "$SDIR"/styles.sh --gruvbox ;;
 				*Dark) "$SDIR"/styles.sh --dark ;;
 				*Cherry) "$SDIR"/styles.sh --cherry ;;
+				*CherryEdit) "$SDIR"/styles.sh --cherrye ;;
             esac
