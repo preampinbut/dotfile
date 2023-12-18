75a7ec0e0d4a1713e4caf818e7bf427da8a5f541
Pream Pinbut <momozahara@hotmail.com>
2023-12-18 19:46:47 +0700

    update i3 and polybar and sleep actions

--- .config/polybar/forest/scripts/powermenu.sh
+++ .config/polybar/forest/scripts/powermenu.sh
@@ -34,7 +34,7 @@ msg() {
 }
 
 # Variable passed to rofi
-options="$hibernate\n$logout\n$reboot\n$shutdown"
+options="$hibernate\n$lock\n$logout\n$reboot\n$shutdown"
 
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
 case $chosen in
@@ -46,23 +46,23 @@ case $chosen in
       ;;
     $lock)
 		if [[ -f /usr/bin/i3lock ]]; then
-			i3lock
+      /usr/bin/light-locker-command -l
 		elif [[ -f /usr/bin/betterlockscreen ]]; then
 			betterlockscreen -l
 		fi
       ;;
-    $suspend)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-      ;;
+  #   $suspend)
+		# ans=$(confirm_exit &)
+		# if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+		# 	mpc -q pause
+		# 	amixer set Master mute
+		# 	systemctl suspend
+		# elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+		# 	exit 0
+  #       else
+		# 	msg
+  #       fi
+  #     ;;
     $hibernate)
       systemctl hibernate
       ;;
@@ -72,8 +72,7 @@ case $chosen in
       elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
         bspc quit
       elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-        # i3-msg exit
-        dm-tool lock
+        i3-msg exit
       fi
       ;;
 esac
--- lib/systemd/system-sleep/actions.sh
+++ lib/systemd/system-sleep/actions.sh
@@ -5,8 +5,5 @@ export XDG_SEAT_PATH=/org/freedesktop/DisplayManager/Seat0
 case "$1" in
     pre)
         /usr/bin/pkill spotifyd
-        /usr/bin/sleep 1
-        /usr/bin/dm-tool switch-to-greeter # protected by light-locker
-        /usr/bin/sleep 1
         ;;
 esac
