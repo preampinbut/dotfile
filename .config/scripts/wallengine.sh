326a71b7ae5cda0ff31f865bc90193b8ee37bd8c
Pream Pinbut <momozahara@hotmail.com>
2023-12-21 16:52:37 +0700

    add wallpaper engine daemon scripts

---
+++ .config/scripts/wallengine.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/preampinbut/.local/bin/wallengine --screen-root eDP-1 --silent --fps $1 /home/preampinbut/.local/share/Steam/steamapps/workshop/content/431960/2891261228
---
+++ .config/scripts/wallenginedaemon.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+prev_power_status=$(cat /sys/class/power_supply/ADP1/online)
+if [ "$prev_power_status" -eq 0 ]; then
+  echo "wallengine 0"
+  /usr/bin/nohup /home/preampinbut/.config/scripts/wallengine.sh 10 &> /dev/null & disown
+elif [ "$prev_power_status" -eq 1 ]; then
+  echo "wallengine 1"
+  /usr/bin/nohup /home/preampinbut/.config/scripts/wallengine.sh 15 &> /dev/null & disown
+fi
+
+while true; do
+  power_status=$(cat /sys/class/power_supply/ADP1/online)
+
+  if [ "$power_status" -ne "$prev_power_status" ]; then
+    echo "kill wallengine"
+    pkill -x wallengine
+    sleep 1
+    if [ "$power_status" -eq 0 ]; then
+      echo "wallengine 0"
+      /usr/bin/nohup /home/preampinbut/.config/scripts/wallengine.sh 10 &> /dev/null & disown
+    elif [ "$power_status" -eq 1 ]; then
+      echo "wallengine 1"
+      /usr/bin/nohup /home/preampinbut/.config/scripts/wallengine.sh 15 &> /dev/null & disown
+    fi
+
+    prev_power_status=$power_status
+  fi
+
+  sleep 3
+done
