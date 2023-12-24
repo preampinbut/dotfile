f02a34283bf4b0ea037e89827cc2bac2f741056e
Pream Pinbut <momozahara@hotmail.com>
2023-12-24 16:37:58 +0700

    update config

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -6,20 +6,14 @@ local plugins = {
   -- Override plugin definition options
   {
     "ThePrimeagen/harpoon",
+    lazy = true,
     branch = "harpoon2",
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
     config = function()
-      local harpoon = require "harpoon"
-      harpoon:setup()
-      harpoon:extend {
-        UI_CREATE = function(cx)
-          vim.keymap.set("n", "l", function()
-            harpoon.ui:select_menu_item()
-          end, { buffer = cx.bufnr })
-        end,
-      }
+      local harpoon = require "custom.configs.harpoon"
+      harpoon.setup()
     end,
   },
   {
