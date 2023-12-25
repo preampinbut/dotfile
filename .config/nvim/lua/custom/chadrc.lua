5dec51b934695a22d166dbae7234c1ab4c61f515
Pream Pinbut <momozahara@hotmail.com>
2023-12-25 19:23:25 +0700

    update missing toggle themes

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tokyodark",
-  theme_toggle = { "tokyodark" },
+  theme = "tokyonight",
+  theme_toggle = { "tokyodark", "tokyonight" },
   transparency = true,
 
   hl_override = highlights.override,
