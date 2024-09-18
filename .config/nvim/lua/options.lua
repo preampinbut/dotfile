2c598b68d86b5afac27033cc159a924053285906
Pream Pinbut <momozahara@hotmail.com>
2024-09-19 03:56:12 +0700

    chore(nvim): add clipboard

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -8,6 +8,7 @@ o.relativenumber = true
 o.wrap = true
 o.showcmd = true
 o.scrolloff = 15
+o.clipboard = "unnamedplus"
 
 vim.g.vscode_snippets_path = vim.fn.expand "$HOME/.config/Code/User/snippets/"
 
