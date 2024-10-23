5230962b254c8631e6d11ba693bd8bec36cd6dd7
Pream Pinbut <momozahara@hotmail.com>
2024-10-23 07:00:20 +0700

    chore(scripts): update pactl-set script

--- .config/scripts/pactl-set.sh
+++ .config/scripts/pactl-set.sh
@@ -2,12 +2,11 @@
 
 max_volume=100 # in percent
 
-pactl subscribe \
-| grep --line-buffered 'sink ' \
-| stdbuf -o0 cut -d# -f2 \
-| while read index; do
-    volume=$(pactl get-sink-volume $index | head -n1 | cut -d/ -f2 | tr -d ' %');
-    if (( volume > max_volume )); then
-        pactl set-sink-volume $index $max_volume%;
-    fi;
-done
+sink="@DEFAULT_SINK@"
+
+pactl set-sink-volume $sink $1
+
+volume=$(pactl get-sink-volume $sink | head -n1 | cut -d/ -f2 | tr -d ' %');
+if (( volume > max_volume )); then
+    pactl set-sink-volume $sink $max_volume%;
+fi;
