f5c21f0b6a81f9a38234f164c045a110a6263cf6
Pream Pinbut <momozahara@hotmail.com>
2024-10-30 16:07:41 +0700

    chore(scripts): update battery script

--- .config/scripts/battery.sh
+++ .config/scripts/battery.sh
@@ -4,8 +4,14 @@ status=$(cat /sys/class/power_supply/BAT0/status)
 energy_full=$(cat /sys/class/power_supply/BAT0/energy_full)
 energy_now=$(cat /sys/class/power_supply/BAT0/energy_now)
 
-threshold=$((energy_full / 10 ))
+# 10%
+threshold_1=$((energy_full / 10 ))
+# 20%
+threshold_2=$((energy_full / 20 ))
 
-if [ "$status" = "Discharging" ] && [ "$energy_now" -lt "$threshold" ]; then
+if [ "$status" = "Discharging" ] && [ "$energy_now" -lt "$threshold_1" ]; then
   /usr/bin/xbacklight -set 25
+elif [ "$status" = "Discharging" ] && [ "$energy_now" -lt "$threshold_2" ]; then
+  systemctl hibernate
 fi
+  
