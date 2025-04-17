d8b97d3f6a9500efb2cfdd03068b9bebfaaec4ef
Pream Pinbut <preampinbut@outlook.co.th>
2025-04-17 13:20:52 +0700

    fix(nvim): conform is a bitch

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -10,16 +10,17 @@ local lspconfig = require "lspconfig"
 
 -- EXAMPLE
 local servers = {
+  -- "lua_ls",
+  -- "cssls",
+  -- "volar",
+  -- "eslint",
+  -- "rust_analyzer",
+  -- "gopls",
   "html",
-  "cssls",
   "tailwindcss",
-  "volar",
   "clangd",
-  "eslint",
   "emmet_ls",
   "jsonls",
-  "rust_analyzer",
-  "gopls",
   "csharp_ls",
   "pyright",
   "dockerls",
@@ -38,17 +39,49 @@ end
 
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+local function buf_format(client, bufnr)
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format()
+    end,
+  })
+  nvlsp.on_attach(client, bufnr)
 end
 
+lspconfig.lua_ls.setup {
+  on_attach = buf_format,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    Lua = {
+      workspace = {
+        library = {
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lazy",
+          "${3rd}/luv/library"
+        }
+      }
+    }
+  }
+}
+
+lspconfig.rust_analyzer.setup {
+  on_attach = buf_format,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    ["rust_analyzer"] = {
+      diagnistics = {
+        enabled = true,
+      },
+    },
+  },
+}
+
 lspconfig.gopls.setup {
-  on_attach = nvlsp.on_attach,
+  on_attach = buf_format,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
   settings = {
@@ -60,7 +93,7 @@ lspconfig.gopls.setup {
 
 local mason_registry = require "mason-registry"
 local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-  .. "/node_modules/@vue/language-server"
+    .. "/node_modules/@vue/language-server"
 
 if ts_ls == true then
   lspconfig.ts_ls.setup {
@@ -113,14 +146,22 @@ lspconfig.cssls.setup {
 }
 
 lspconfig.eslint.setup {
-  flags = {
-    allow_incremental_sync = false,
-    debounce_text_changes = 1000,
-  },
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+    nvlsp.on_attach(client, bufnr)
+  end,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
 }
 
-lspconfig.tailwindcss.setup {
-  flags = {
-    debounce_text_changes = 1000,
-  },
-}
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
