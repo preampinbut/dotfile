21fea2d7350f97efa389b403933881a586ae9114
Pream Pinbut <preampinbut@outlook.co.th>
2025-11-19 22:07:12 +0700

    chore(nvim_plugins): remove markdown-preview.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -92,14 +92,4 @@ return {
       require "configs.indent"
     end,
   },
-
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
 }
