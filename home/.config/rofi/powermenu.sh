3fc27d72dd05349de70bbf66538a52d818caf084
Pream Pinbut <preampinbut@outlook.co.th>
2025-12-04 22:51:36 +0700

    chore: format

--- home/.config/rofi/powermenu.sh
+++ home/.config/rofi/powermenu.sh
@@ -19,12 +19,12 @@ shutdown=" Shutdown"
 
 # Confirmation
 confirm_exit() {
-    echo -e "No\nYes" | rofi -dmenu \
-        -no-config \
-        -i \
-        -no-fixed-num-lines \
-        -p "Do You Want To $1?" \
-        -theme "$HOME/.config/rofi/themes/powermenu-rounded-pink-dark.rasi"
+  echo -e "No\nYes" | rofi -dmenu \
+    -no-config \
+    -i \
+    -no-fixed-num-lines \
+    -p "Do You Want To $1?" \
+    -theme "$HOME/.config/rofi/themes/powermenu-rounded-pink-dark.rasi"
 }
 
 # Variable passed to rofi
@@ -33,42 +33,42 @@ options="$lock\n$logout\n$suspend\n$hibernate\n$reboot\n$shutdown"
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
 
 case $chosen in
-    $shutdown)
-      ans=$(confirm_exit Shutdown &)
-      if [[ "$ans" == "Yes" ]]; then
-        systemctl poweroff
-      fi
-      ;;
-    $reboot)
-      ans=$(confirm_exit Reboot &)
-      if [[ "$ans" == "Yes" ]]; then
-        systemctl reboot
-      fi
-      ;;
-    $lock)
-      ans=$(confirm_exit Lock &)
-      if [[ "$ans" == "Yes" ]]; then
-        $HOME/.config/scripts/lock
-      fi
-      ;;
-    $hibernate)
-      ans=$(confirm_exit Hibernate &)
-      if [[ "$ans" == "Yes" ]]; then
-        systemctl hibernate
-      fi
-      ;;
-    $suspend)
-      ans=$(confirm_exit Suspend &)
-      if [[ "$ans" == "Yes" ]]; then
-        mpc -q pause
-        amixer set Master mute
-        systemctl suspend
-      fi
-      ;;
-    $logout)
-      ans=$(confirm_exit Logout &)
-      if [[ "$ans" == "Yes" ]]; then
-        sway exit
-      fi
-      ;;
+$shutdown)
+  ans=$(confirm_exit Shutdown &)
+  if [[ "$ans" == "Yes" ]]; then
+    systemctl poweroff
+  fi
+  ;;
+$reboot)
+  ans=$(confirm_exit Reboot &)
+  if [[ "$ans" == "Yes" ]]; then
+    systemctl reboot
+  fi
+  ;;
+$lock)
+  ans=$(confirm_exit Lock &)
+  if [[ "$ans" == "Yes" ]]; then
+    /bin/loginctl lock-session "$XDG_SESSION_ID"
+  fi
+  ;;
+$hibernate)
+  ans=$(confirm_exit Hibernate &)
+  if [[ "$ans" == "Yes" ]]; then
+    systemctl hibernate
+  fi
+  ;;
+$suspend)
+  ans=$(confirm_exit Suspend &)
+  if [[ "$ans" == "Yes" ]]; then
+    mpc -q pause
+    amixer set Master mute
+    systemctl suspend
+  fi
+  ;;
+$logout)
+  ans=$(confirm_exit Logout &)
+  if [[ "$ans" == "Yes" ]]; then
+    sway exit
+  fi
+  ;;
 esac
