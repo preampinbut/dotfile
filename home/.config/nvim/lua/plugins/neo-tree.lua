b2dc2e6c3c3a805005a1a373623d1f40b1fdd199
Pream Pinbut <preampinbut@outlook.co.th>
2025-12-10 20:17:18 +0700

    update: neo-tree keys

--- home/.config/nvim/lua/plugins/neo-tree.lua
+++ home/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,32 @@ return {
   -- source https://www.lazyvim.org/extras/editor/neo-tree
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+      {
+        "<leader>be",
+        function()
+          require("neo-tree.command").execute({ source = "buffers", toggle = true })
+        end,
+        desc = "Buffer Explorer",
+      },
+      { "<leader>B", "<leader>be", desc = "Buffer Explorer", remap = true },
+    },
     opts = {
       filesystem = {
         filtered_items = {
