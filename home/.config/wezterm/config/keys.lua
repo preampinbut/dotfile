98891aebc8a6d055b0f8d1b390bcd260c910dd03
Pream Pinbut <preampinbut@outlook.co.th>
2025-12-21 02:07:10 +0700

    update: wezterm keys

--- home/.config/wezterm/config/keys.lua
+++ home/.config/wezterm/config/keys.lua
@@ -1,11 +1,16 @@
 return function(wezterm, config)
-  config.keys = {
-    {
-      key = "w",
-      mods = "CMD",
-      action = wezterm.action.CloseCurrentTab({ confirm = false }),
-    },
-  }
+	config.keys = {
+		{
+			key = "w",
+			mods = "CMD",
+			action = wezterm.action.CloseCurrentTab({ confirm = false }),
+		},
+		{
+			key = "Enter",
+			mods = "SHIFT",
+			action = wezterm.action({ SendString = "\x1b\r" }),
+		},
+	}
 
-  return config
+	return config
 end
